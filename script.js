a10be0e8d50d12516d4afc1a0ee8a750b68a3729
Ankita <ankietathorve@gmail.com>
2026-05-01 15:49:34 +0530

    function to pass attendese

--- script.js
+++ script.js
@@ -20,4 +20,11 @@ function initializeThroughput(gates) {
   return summary;
 }
 
-
+function processGateFlow(gate, tickIndex) {
+  let currentTickQueue = gate.queue[tickIndex];
+  let processed = 0;
+  while (currentTickQueue > 0 && processed < gate.capacity) {
+    currentTickQueue--;
+    processed++;
+  }
+}
